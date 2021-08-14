4ee1ca501904219e1ed2382135b542824057dc47
Bereket Kibru <bekakibru2@gmail.com>
2021-08-14 11:24:24 +0300

    add prediction demo

---
+++ src/pages/pred.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def write():
+    st.title(" Swahili Speech To Text ")
+    st.write("""
+    # Prediction Demo
+    """)
+    st.video("images/f0e383c7-277f-4d0e-97be-666e34c26540.webm")
